25fee7f18c12eb2921e25f767392c580895de22e
kaaassspeeerrr <64116042+kaaassspeeerrr@users.noreply.github.com>
2022-12-23 21:29:47 -0500

    Update ArduinoSketch.ino
    New algorithm for light changing. Actually works!
    
    There is a bug where it seems to preform all light changes 2 times.

--- ArduinoSketch.ino
+++ ArduinoSketch.ino
@@ -1,106 +1,121 @@
-//define pins
-int usTrigPin = 9;
-int usEchoPin = 8;
-int warmLed = 6;
-int coolLed = 5;
+/*
+ * Wiring: Ultrasonic Sensor -> Arduino:
+ * - VCC -------> 5VDC
+ * - TRIG ------> Pin 9
+ * - ECHO ------> Pin 8
+ * - GND -------> GND
+ *
+ * Wiring: CCT LED Strip -> Arduino:
+ * - Warm LEDs -> Pin 6
+ * - Cool LEDs -> Pin 5
+ * - GND -------> GND
+ */
 
-//define individual brightness values for the warm and cool LEDs
-int wBright = 255; 
-int cBright = 0;
+int trigPin = 9;  // TRIG pin
+int echoPin = 8;  // ECHO pin
+int wLedPin = 6;  // ECHO pin
+int cLedPin = 5;  // ECHO pin
 
-//define fade speed
-int fadeSpeed = 10;
+float duration_us = 0;   // Ultra-sonic sensor output
+float distance_cm = 0;   // Ultra-sonic sensor output converted to cm
+float distance_x = 0;    // Last measured distance in cm
+float distance_y = 0;    // Second to last measured distance in cm
+float distance_z = 0;    // Third to last measured distance in cm
+float distance_avg = 0;  // The Average of the last three measured distances in cm
 
-//define bed state
-int bedState = 0;
+int warmBrightness = 0;  // Initial brightness value for all warm LEDs
+int coolBrightness = 0;  // Initial brightness value for all cool LEDs
 
-int duration_us = 0;
-int distance_cm = 0;
-
+int fadeSpeed = 10;  //Delay in milliseconds between each brightness incrementation
 
 void setup() {
+  // begin serial port
+  Serial.begin(9600);
 
-// begin serial port
-Serial.begin (9600);
-
-//set up pins to output or input
-pinMode(usTrigPin, OUTPUT);
-pinMode(usEchoPin, INPUT);
-pinMode(warmLed, OUTPUT);
-pinMode(coolLed, OUTPUT);
+  // configure the trigger pin to output mode
+  pinMode(trigPin, OUTPUT);
+  // configure the echo pin to input mode
+  pinMode(echoPin, INPUT);
+  // configure the warm LED pin to output mode
+  pinMode(wLedPin, OUTPUT);
+  // configure the cool LED pin to output mode
+  pinMode(cLedPin, OUTPUT);
+}
+void loop() {
+  ultraSonic();
 }
 
-void loop(){
-delay (500);
-  UltraSonic();
-
-  if (bedState = 0){
-BedGoesUp();
-  }
-  else if (bedState = 1){
-BedGoesDown();
-  }
-  
-}
-
- void UltraSonic(){
+void ultraSonic() {
   // generate 10-microsecond pulse to TRIG pin
-  digitalWrite(usTrigPin, HIGH);
-  delayMicroseconds(500);
-  digitalWrite(usTrigPin, LOW);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
 
   // measure duration of pulse from ECHO pin
-  duration_us = pulseIn(usEchoPin, HIGH);
+  duration_us = pulseIn(echoPin, HIGH);
 
   // calculate the distance
   distance_cm = 0.017 * duration_us;
+  distance_z = distance_y;
+  distance_y = distance_x;
+  distance_x = distance_cm;
+  distance_avg = ((distance_x + distance_y + distance_z) / 3);
 
-  // print the value to Serial Monitor
-  // Serial.print("distance: ");
-  // Serial.print(distance_cm);
-  // Serial.println(" cm");
-
-  delay (500);
-
-  if (distance_cm > 20){
-    bedState = 0;
+  // if the bed is up
+  if (distance_avg <= 11) {
+    Serial.print("Bed is up. ");
+    // if the lights are already cool
+    if (coolBrightness == 255) {
+      Serial.print("Lights are already cool. ");
+      // print the value to Serial Monitor
+      Serial.print("Avg Distance: ");
+      Serial.print(distance_avg);
+      Serial.println(" cm");
+      delay(1000);
     }
-  else if (distance_cm < 20){
-    bedState = 1;
+    //if the lights are not already cool
+    else {
+      Serial.println("Raising color Temperature. ");
+      for (int i = 0; i < 255; i++) {
+        // turn off the warm LEDs
+        analogWrite(wLedPin, warmBrightness);
+        warmBrightness -= 1;
+
+        // turn on the cool LEDs
+        analogWrite(cLedPin, coolBrightness);
+        coolBrightness += 1;
+
+        delay(fadeSpeed);
+      }
     }
+  }
+  // if the bed is down
+  else {
+    Serial.print("Bed is down. ");
+    // if the lights are already warm
+    if (warmBrightness == 255) {
+      Serial.print("Lights are already warm. ");
+      // print the value to Serial Monitor
+      Serial.print("Avg Distance: ");
+      Serial.print(distance_avg);
+      Serial.println(" cm");
+      delay(1000);
+    }
+    //if the lights are not already warm
+    else {
+      Serial.println("Lowering Color Temperature.");
+      for (int i = 0; i < 255; i++) {
+        // turn on the warm LEDs
+        analogWrite(wLedPin, warmBrightness);
+        warmBrightness += 1;
+
+        // turn off the cool LEDs
+        analogWrite(cLedPin, coolBrightness);
+        coolBrightness -= 1;
+        
+        delay(fadeSpeed);
+      }
+    }
+  }
+
 }
-
-void BedGoesUp(){
-    for (int i=0;i<256; i++){
-      
-      analogWrite(warmLed, wBright);
-      wBright -=1;
-      delay(fadeSpeed);
-
-    }
-    
-    for (int i=0;i<256; i++){
-      
-      analogWrite(coolLed, cBright);
-      cBright += 1;
-      delay(fadeSpeed);
-    }    
-}
-
-void BedGoesDown(){
-    for (int i=0;i<256; i++){
-      
-      analogWrite(warmLed, wBright);
-      wBright +=1;
-      delay(fadeSpeed);
-
-    }
-    
-    for (int i=0;i<256; i++){
-      
-      analogWrite(coolLed, cBright);
-      cBright -= 1;
-      delay(fadeSpeed);
-    }
-
-    }
